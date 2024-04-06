fc8cb11b800da759f7f18d4b3f5a31b98f163b54
Harshith Kumar <harshith.gannavarapu23@gmail.com>
2024-04-06 11:21:30 +0530

    header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
+import About from './pages/About'
+import Login from './pages/Login'
+import Jobs from './pages/Jobs'
+import Search from './pages/Search'
 
 export default function App() {
   return (
@@ -9,6 +13,10 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Login />} />
+        <Route path='/job' element={<Jobs />} />
+        <Route path='/search' element={<Search />} />
       </Routes>
     </BrowserRouter>
   )
